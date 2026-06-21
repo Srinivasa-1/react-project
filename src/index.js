f2b45f6f2997aa4a75410f5fbd344f63ead58531
Srinivasa <nsrinivasa871@gmail.com>
2026-06-21 10:54:45 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import react from "react";
+import ReactDOM from "react-dom/client";
+
+let el = document.getElementById("root");
+
+const root = ReactDOM.createRoot(el);
+
+function App(){
+  return <h1>Hello this is srinivasa N</h1>
+}
+
+
+root.render(<App/>)
